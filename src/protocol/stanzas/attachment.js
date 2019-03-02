77433d61d19521ba98d2386f4ae7cdd85b42fe26
Cricket La Chica <hello@cricketlachica.com>
2019-03-01 15:26:14 -1000

    Implementing Raftr specific items.

--- src/protocol/stanzas/attachment.js
+++ src/protocol/stanzas/attachment.js
@@ -1,18 +1,18 @@
-import * as NS from '../namespaces'
+import * as NS from '../namespaces';
 
 export default function(JXT) {
-  const Utils = JXT.utils
+    const Utils = JXT.utils;
 
-  const Attachment = JXT.define({
-    element: 'attachment',
-    fields: {
-      dispay_width: Utils.attribute('dispay_width'),
-      display_height: Utils.attribute('display_height'),
-      type: Utils.attribute('type'),
-    },
-    name: 'attachment',
-    namespace: '',
-  })
+    const Attachment = JXT.define({
+        element: 'attachment',
+        fields: {
+            dispay_width: Utils.attribute('dispay_width'),
+            display_height: Utils.attribute('display_height'),
+            type: Utils.attribute('type')
+        },
+        name: 'attachment',
+        namespace: ''
+    });
 
-  JXT.extendMessage(Attachment)
+    JXT.extendMessage(Attachment);
 }
